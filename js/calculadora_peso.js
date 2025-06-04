ea20d345e4c31f30c2e1d58af9532b6a301ede53
serpa-hub <yojanerserpa1@gmail.com>
2025-06-04 13:16:21 -0500

    Delete js directory

--- js/calculadora_peso.js
+++
@@ -1,9 +0,0 @@
-const pesoDeReferencia = 70; // constante para peso de referencia
-let peso = parseFloat(prompt("¿Cuánto pesas en kilogramos?"));
-let pesoEnGramos = peso * 1000;
-var esMayor70 = peso > pesoDeReferencia;
-let combinacionLogica = esMayor70 && true;
-
-console.log("Peso en gramos:", pesoEnGramos);
-console.log("¿Es mayor a 70 kg?", esMayor70);
-console.log("Combinación lógica:", combinacionLogica);
